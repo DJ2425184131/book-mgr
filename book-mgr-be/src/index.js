c0970a20ff705e927bfdd45a9e9e257b5123da56
董静 <2425184131@qq.com>
2022-01-09 20:01:50 +0800

    分支测试

--- book-mgr-be/src/index.js
+++ book-mgr-be/src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-04 08:50:54
- * @LastEditTime: 2022-01-07 10:36:54
+ * @LastEditTime: 2022-01-09 20:00:20
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \projects\book-mgr-be\src\index.js
@@ -88,4 +88,4 @@ app.use(async (context,next)=>{
 app.listen(3000,()=>{
     console.log('启动成功');
 });
-// console.log('123456'); 
+console.log('123456'); 
