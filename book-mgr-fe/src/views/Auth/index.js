1e4d70eb15abccc5c04bace74bdc21d35ddb8ff7
董静 <2425184131@qq.com>
2022-01-18 13:26:07 +0800

    '完成前端登入注册相关的界面'

---
+++ book-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,13 @@
+import { defineComponent } from 'vue';
+import { UserOutlined,LockOutlined,MailOutlined } from '@ant-design/icons-vue';
+
+export default defineComponent({
+    components:{
+        UserOutlined,
+        LockOutlined,
+        MailOutlined,
+    },
+    setup(){
+        
+    },
+});
